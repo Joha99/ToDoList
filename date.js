af252a47d8d96f1d1cc6b802a5fc5981da0c1e57
Joha Kim <johakim9936@gmail.com>
2020-06-30 12:18:41 -0400

    small changes

--- date.js
+++ date.js
@@ -1,11 +1,11 @@
-module.exports = getDate; 
+module.exports.getDate = getDate; 
 
 function getDate() {
   let today = new Date();
 
   let options = { weekday: "long", day: "numeric", month: "long" };
 
-  let day = today.toLocaleDateString("en-US", options);
+  let date = today.toLocaleDateString("en-US", options);
 
-  return day;
+  return date;
 }
